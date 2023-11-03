b484e12bda11565ff6c0ed413dc104d285686948
Drishya <drishyaa005@gmail.com>
2023-11-03 14:10:17 +0545

    testfile

---
+++ testing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar} from './components'
+
+const App = () =>(
+    <div className='App'>
+        hello
+        <Navbar/>
+    </div>
+)
+
+export default App
+
