0d5782316cad9556115932977482261304322ec7
Drishya <drishyaa005@gmail.com>
2023-11-06 16:54:52 +0545

    gpt3 website done

--- website1/cal/src/containers/possibility/Possibility.jsx
+++ website1/cal/src/containers/possibility/Possibility.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 const Possibility = () => {
   return (
-    <div>
-      possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis sit ducimus minima, aliquid voluptas facilis cumque distinctio aut voluptate reiciendis velit odit eaque eveniet ipsa molestiae beatae unde dolore hic.</p>
+        <h4>Request Early Access to Get Started!</h4>
+      </div>
     </div>
   )
 }
